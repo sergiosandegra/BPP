56a36c5fadfa3bc3c69b02cbd316695b0fa96a61
sergiosandegra <sergiosanchezdegracia00@gmail.com>
2022-01-06 21:36:25 +0100

    PDF

--- Leccion_3/actividad_leccion_3/doc/source/modules.rst
+++
@@ -1,7 +0,0 @@
-ope
-===
-
-.. toctree::
-   :maxdepth: 4
-
-   ope
--- Leccion_3/actividad_leccion_3/ope/__main__.py
+++
@@ -1,5 +0,0 @@
-from utils import actividadOperaciones
-
-if __name__ == "__main__":
-    m = actividadOperaciones.OperacionesMatematicas(6, 4)
-    print(m.suma())
