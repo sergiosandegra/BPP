0250f2acca7ced94835ce3a251d3e0924d428c01
sergiosandegra <sergiosanchezdegracia00@gmail.com>
2022-01-06 20:07:25 +0100

    import pdb

--- Leccion_4/ejercicio1Leccion4.py
+++ Leccion_4/ejercicio1Leccion4.py
@@ -14,8 +14,13 @@ mostrando el contenido de las variables y continuar con la ejecución
 línea a línea. ¿Qué conclusiones obtiene?
 '''
 
+import pdb
+pdb.set_trace()
+
 numeros = [[2, 4, 1], [1, 2, 3, 4, 5, 6, 7, 8], [100, 250, 43]]
 
 solucion = [max(i) for i in numeros]
 
 print("El mayor elemento de cada sublista es:", solucion)
+
+# CONCLUSIÓN
